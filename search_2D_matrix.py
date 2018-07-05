bfc0b92aa1e2b7e04f881673d64ba3b69f5f98f5
Karthik Iyer <karthik2@gmx.com>
2018-07-05 11:32:12 -0400

    Initial commit

---
+++ search_2D_matrix.py
@@ -0,0 +1,67 @@
+'''Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+[
+  [1, 4, 7, 11, 15],
+  [2, 5, 8, 12, 19],
+  [3, 6, 9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+
+Given target = 5, return true.
+
+Given target = 20, return false.
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+
+'''
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        right = len(matrix[0]) - 1
+        down = 0
+        while down < len(matrix) and right >= 0:
+            if matrix[down][right] > target:
+                right -= 1
+            elif matrix[down][right] < target:
+                down += 1
+            else:
+                return True
+        return False
+        
+        # Snake through the array from the top right! Same solution works for both types of sorted array 
